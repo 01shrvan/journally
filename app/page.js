dfdaabadf60b9bd900416d20dabdb6963bc54439
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-03-08 19:18:12 +0530

    feat(ui): add Skeleton, Textarea, Toaster, Card, Badge components and update Button variant

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>helooooo</div>;
+  return (
+    <div>
+      helooooo
+      <br />
+      <Button variant="journal">helo</Button>
+    </div>
+  );
 }
