7a6bad01254f047fbecd4271396e779636698791
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-03-10 18:12:05 +0530

    feat(ui): update Home page with new features section, enhance layout and add FAQ data

--- app/layout.js
+++ app/layout.js
@@ -14,21 +14,21 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className}`}>
+        <body className={`${inter.className} text-orange-900`}>
           <div className="bg-[url('/bg.jpg')] opacity-50 fixed -z-10 inset-0" />
           <Header />
           <main className="min-h-screen">{children}</main>
           <footer className="bg-orange-200 py-6 bg-opacity-10 flex justify-between items-center px-4">
             <div className="text-left">
-              <p className="text-gray-900">
+              <p>
                 Capture your thoughts, one day at a time.
               </p>
             </div>
-            <div className="text-right text-gray-900">
-              Made with <span>&#10084;</span> by{" "}
+            <div className="text-right">
+              Made with <span className="text-orange-900">&#10084;</span> by{" "}
               <a
                 href="https://x.com/01shrvan"
-                className="text-gray-900 relative font-medium hover:text-gray-800 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-gray-800 after:left-0 after:bottom-0 after:transition-all after:duration-300 hover:after:w-full"
+                className="relative font-medium hover:text-orange-900 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-orange-900 after:left-0 after:bottom-0 after:transition-all after:duration-300 hover:after:w-full"
               >
                 shrvan
               </a>
